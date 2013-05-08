300f58e667345e7827c6dfc2f7a6dd79ff7e0cf6
steeve cannon <stephentcannon@gmail.com>
2013-05-08 19:28:10 -0400

    fixing indents

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 Meteor.startup(function () {
-	Meteor.users.update({}, {$set:{active: false}}, {multi: true});
+  Meteor.users.update({}, {$set:{active: false}}, {multi: true});
 });
 
 
 Accounts.preLogin(function(options){
-	console.log('********** Accounts.preLogin *************');
-	console.log(options); //if you want to see the options passed
-	// this demonstrates using preLogin to prevent inactive users trying to resume a session
+  console.log('********** Accounts.preLogin *************');
+  console.log(options); //if you want to see the options passed
+  // this demonstrates using preLogin to prevent inactive users trying to resume a session
   if(options.resume){
-  	if(!Meteor.users.findOne({'services.resume.loginTokens.token': ""+options.resume, active: true})){
+    if(!Meteor.users.findOne({'services.resume.loginTokens.token': ""+options.resume, active: true})){
       return false;
     } 
   } 
@@ -16,9 +16,9 @@ Accounts.preLogin(function(options){
 });
 
 Accounts.postLogin(function(result){
-	console.log('************* Accounts.postLogin ********');
-	console.log(result); //if you want to see the results
-	// this demonstrates using postLogin to prevent inactive user
+  console.log('************* Accounts.postLogin ********');
+  console.log(result); //if you want to see the results
+  // this demonstrates using postLogin to prevent inactive user
   if(!Meteor.users.findOne({_id: result.id, active: true})){
     return false;
   } else {
